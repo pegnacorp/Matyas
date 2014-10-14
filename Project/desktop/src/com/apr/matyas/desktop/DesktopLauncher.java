e1ac900ce02d1d757cbb60309eea36ee527e08f6
apr159 <apr_159@yahoo.com>
2014-10-13 19:30:14 -0500

    Added gradle

---
+++ Project/desktop/src/com/apr/matyas/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.apr.matyas.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.apr.matyas.MatyasGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+
+		
+		new LwjglApplication(new MatyasGame(), config);
+	}
+}
---
+++ Project/html/src/com/apr/matyas/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.apr.matyas.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.apr.matyas.MatyasGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MatyasGame();
+        }
+}
